2e2dfcdb81f4cc1c711318fc8786df2450b064a1
siaeeisHounde <stgu90osa6165@126.com>
2021-03-11 14:55:02 +0800

    add new funtion for cards

---
+++ js/app1.js
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+  Init();//初始化
+  $("div.restart").click(function () {
+    Init();//初始化
+  });
+
+  $("li").click(function(evt) {
+
+      if (evt) {
+       var thisSquare = evt.target;
+      } else {
+        var thisSquare = window.event.srcElenment;
+      }
+      if (thisSquare.className == "card") {
+        $(this).addClass("card open show");
+        }
+
+  })
+  }
+);
+
+
+Init = function(){
+  $("ul").children().removeClass().addClass("card");
+  $("#stars-three").find("i").removeClass();
+  $("#squar").find("i").removeClass();
+  for(var i= 0; i < 16; i++){
+    $("#square"+i).addClass(cards[i]);
+  };
+
+};//初始化函数
+
+var cards = ["fa fa-diamond","fa fa-diamond",
+    "fa fa-paper-plane-o","fa fa-paper-plane-o",
+    "fa fa-anchor","fa fa-anchor",
+    "fa fa-bolt","fa fa-bolt",
+    "fa fa-cube","fa fa-cube",
+    "fa fa-leaf","fa fa-leaf",
+    "fa fa-bicycle","fa fa-bicycle",
+    "fa fa-bomb","fa fa-bomb"];
+ /*changCards = function{
+$("#square0").removeClass().addClass(cards[7]);
+};*/
