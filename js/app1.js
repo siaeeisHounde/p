3e2fd42d136d80f8a44cab50a2a2222dbd84392f
siaeeisHounde <stgu90osa6165@126.com>
2021-03-26 14:24:36 +0800

    change new code for JQ

--- js/app1.js
+++ js/app1.js
@@ -12,14 +12,12 @@ $(document).ready(function() {
         } else {
             var thisSquare = window.event.srcElenment;
         }
-        if(thisSquare.className == "card") {
-            $(this).addClass("card open show")
-            .fadeTo(400,0.25).fadeTo(100,1);//产生一个动画效果。
-            //alert($(this).find("i").prop("className"));
-
-
-          }
-
+          //alert($(this).find("i").arrt("id"));
+          //CardPic = $(this).find("i").prop("className");
+          //alert($(this).find("i").prop("className"));
+          //if(checkCards(CardPic)){
+          //  $(this).removeClass().addClass("card");
+        //  }
 
     });
 });
@@ -44,8 +42,8 @@ var cards = new Array("fa fa-diamond",
     "fa fa-cube",
     "fa fa-leaf",
     "fa fa-bicycle",
-    "fa fa-bomb",
-);
+    "fa fa-bomb");
+
 //生成新的卡片布局。
 function newCards() {
 
@@ -59,8 +57,8 @@ function newCards() {
         } while (usedNums[x] < 0); //产生一个合理的随机数字，提供给下面的数组，目的是使卡片图案分配合理。
         $("#square" + i).addClass(cards[Pic]); //给每个卡片一个随机的图形。
         usedNums[x] = -1;
-    };
-};
+    }
+}
 //清除原有布局
 function cleanCards() {
     $("#squar")
@@ -68,7 +66,31 @@ function cleanCards() {
         .find("i").removeClass(); //同时清除html文档里的i标签下的图形。
     $("#stars-three").find("i").removeClass(); //初始化星星。
 }
-//产生一个0到15之间的随机整数
+//产生一个0到16之间的随机整数
 function setSquare() {
     return Math.floor(Math.random() * 16);
+}
+
+//检查两张卡片是否相同，如果相同保留，如果不相同再次遮盖起来。
+
+var first = 0;
+var second = 0;
+var i = 0;
+function checkCards(CardPic) {
+if(first === 0 && second === 0 && i === 0) {
+        first = CardPic;
+        i++;
+    } else {
+        second = CardPic;
+        if(first == second) {
+            return true;
+        } else {
+            return false;
+            first = 0;
+            second = 0;
+            i = 0;
+
+        }
+    }
+
 }
